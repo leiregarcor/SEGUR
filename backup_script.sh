5daf753e6f1666a0c84145248882041a6c886c04
leiregarcor <62213101+leiregarcor@users.noreply.github.com>
2021-10-14 18:06:00 +0200

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Segurtasuna karpetako babes-kopia inkrementala egiten du, egunero eguerdiko 12-tan (Egun bakoitza karpeta bat).
+# Segurtasuna karpetako babes-kopia inkrementala egiten du, egunero eguerdiko 12-tan (Egun bakoitza karpeta bat, hau crontab-en konfiguratzen da).
+# Egilea: Leire Garcia Corcuera
 
 
 rsync -av --link-dest=/var/tmp/Backups/$(date --date=yesterday +"%F") /home/leire/Escritorio/SEGUR/lab/lab1/segurtasuna/ /var/tmp/Backups/$(date '+%F')
