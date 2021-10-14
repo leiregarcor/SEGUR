d72bc83b4ae9dc60dc3ccc6de508b65ef70c5e0a
leiregarcor <62213101+leiregarcor@users.noreply.github.com>
2021-10-14 18:04:59 +0200

    Update aldatuDa.sh

--- aldatuDa.sh
+++ aldatuDa.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Egilea: Leire Garcia Corcuera
+# Programa honek pantailaratzen du proba.txt fitxategia aldatu den ala ez, 
+# horretarako igorleak proba.txt fitxategiarekin batera 
+# probamd5.txt fitxategia bidaliko du hartzaileak frogatu ahal izateko.
+# Erabiltzaileak alda ditzake programan erreferentziatzen diren fitxategiak bere egoerara egokitzeko
+
     var=$(cat probamd5.txt)
     fitxategia=$(md5sum proba.txt)
 	if [[ $var =~ $fitxategia ]]; then
