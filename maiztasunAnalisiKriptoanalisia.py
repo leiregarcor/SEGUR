baae33a1548bd3ffc0607204e03f321e14c7a44c
leiregarcor <62213101+leiregarcor@users.noreply.github.com>
2021-10-14 18:12:50 +0200

    Update maiztasunAnalisiKriptoanalisia.py

--- maiztasunAnalisiKriptoanalisia.py
+++ maiztasunAnalisiKriptoanalisia.py
@@ -1,5 +1,10 @@
-#EGILEA: Leire Garcia
 from collections import Counter
+
+# EGILEA: Leire Garcia
+# Programaren helburua maiztasun analisiaz baliatuz mezu baten kriptoanalisia egitea da.
+# Horretarako programak testuaren hizkien maiztasun analisisa egiten du, eta gehien erabiltzen 
+# diren hizkiak automatikoki aldatzen ditu gaztelaniaz gehien errepikatzen diren hizkiegaz.
+# Gero erabiltzaileak hizkiak aldatzen joan beharko da, testua deszifratu arte.
 
 def aldaketakEgin(ema):
     h1=input("Idatzi aldatu nahi duzun karakterea.")
@@ -8,7 +13,6 @@ def aldaketakEgin(ema):
 
 def main():
 # Testua programan sartu
-#TODO jarri daiteke txt fitxategi bat igotzea programara, programak hura irakurtzea eta deszifratzea
 
     textua = "RIJ AZKKZHC PIKCE XT ACKCUXJHX SZX, E NZ PEJXKE, PXGIK XFDKXNEQE RIPI RIPQEHCK ET OENRCNPI AXNAX ZJ RKCHXKCI AX CJAXDXJAXJRCE AX RTENX, E ACOXKXJRCE AXT RITEQIKERCIJCNPI OKXJHXDIDZTCNHE AX TE ACKXRRCIJ EJEKSZCNHE.      AZKKZHC OZX ZJ OERHIK AX DKCPXK IKAXJ XJ XT DEDXT AX TE RTENX IQKXKE XJ REHETZJVE XJ GZTCI AX 1936. DXKI AZKKZHC, RIPI IRZKKX RIJ TEN DXKNIJETCAEAXN XJ TE MCNHIKCE, JI REVI AXT RCXTI. DXKNIJCOCREQE TE HKEACRCIJ KXvITZRCIJEKCE AX TE RTENX IQKXKE. NZ XJIKPX DIDZTEKCAEA XJHKX TE RTENX HKEQEGEAIKE, KXOTXGEAE XJ XT XJHCXKKI PZTHCHZACJEKCI XJ QEKRXTIJE XT 22 AX JIvCXPQKX AX 1936, PZXNHKE XNE CAXJHCOCRERCIJ. NZ PZXKHX OZX NCJ AZAE ZJ UITDX IQGXHCvI ET DKIRXNI KXvITZRCIJEKCI XJ PEKRME. NCJ AZKKZHC SZXAI PEN TCQKX XT REPCJI DEKE SZX XT XNHETCJCNPI, RIJ TE RIPDTCRCAEA AXT UIQCXKJI AXT OKXJHX DIDZTEK V AX TE ACKXRRCIJ EJEKSZCNHE, HXKPCJEKE XJ PEVI AX 1937 TE HEKXE AX TCSZCAEK TE KXvITZRCIJ, AXNPIKETCLEJAI E TE RTENX IQKXKE V OERCTCHEJAI RIJ XTTI XT DINHXKCIK HKCZJOI OKEJSZCNHE."
     hizkiak = Counter(textua) #txetuan hizki bakoitza zenbat aldiz agertzen den ikusi
