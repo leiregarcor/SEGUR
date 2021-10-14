7224845bb6f893e1d32102da5da688ac605f3e78
leiregarcor <62213101+leiregarcor@users.noreply.github.com>
2021-10-14 18:09:02 +0200

    Update laburpen_script.sh

--- laburpen_script.sh
+++ laburpen_script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# Egilea: Leire Garcia Corcuera
+# Programa honek irudia/ direktorio barruan dauden argazki guztien md5sum egiten du, 
+# gero, lortzen den hash-a alderatzen da aurkitu nahi den argazkiaren hash-arekin,
+# behin argazkia(k) aurkituta programak zein irudi izan den pantailaretzen du.
+
 for var in ./irudia/* ;
- do
- 	
+ do 	
     	irudi=$(md5sum $var)
 	md5="e5ed313192776744b9b93b1320b5e268" 
 
 	if [[ $irudi =~ $md5 ]]; then
 		echo "EUREKA!!!! Artxiboa arkitu duzu!!!!!!!!!"
-		echo  "$var"		
-	
+		echo  "$var"	
 	fi	
 
 done
